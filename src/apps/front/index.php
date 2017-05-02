f502840c412320281839df185ef75b58189090e8
luopan <luopan@w1.dev.aly.mararun.cn>
2017-05-02 18:03:05 +0800

    first modify

--- src/apps/front/index.php
+++ src/apps/front/index.php
@@ -1,9 +1,2 @@
 <?php
-require_once('pylon/pylon.php');  
-/*
-pylon_using_module("/home/q/php/svc_utls");
-$xhprof = new XhprofSvc();
-$xhprof->autoRecode(); 
- */
-
-XPylon::rest_serving('mara_cron');
+require_once('pylon/pylon.php');
