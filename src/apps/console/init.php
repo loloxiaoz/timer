2421206e0fbc2715ca68a53b62daaa3bf0227909
luopan <luopan@w1.dev.aly.mararun.cn>
2017-05-02 18:55:13 +0800

    norm code style

--- src/apps/console/init.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once('pylon/pylon.php');  
-XPylon::console_serving("mara_cron");
-
--- src/apps/front/assembly.php
+++
@@ -1,9 +0,0 @@
-<?php
-class front_assembly
-{
-    static public function setup($cacheSvc=null)
-    {
-        CommonAssembly::setup($cacheSvc);
-        XAop::pos(XAop::LOGIC)->append_by_match_uri(".*", new RebuildRequestParams);
-    }
-}
